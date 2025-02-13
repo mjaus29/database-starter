815d1843dbcf1d1ffc7928c0b7de99d71baf3932
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-13 13:28:29 +0800

    update Prisma client configuration to include PgBouncer and improve readability

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,14 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error", "warn"],
     datasources: {
       db: {
-        url: process.env.DATABASE_URL, // Ensure this is set correctly
+        url:
+          process.env.DATABASE_URL +
+          "?pgbouncer=true&statement_cache_mode=describe",
       },
     },
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}

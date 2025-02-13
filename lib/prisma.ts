ac37b88b15be8fe0644e83fe262feb8c4e812a19
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-13 12:33:52 +0800

    enhance Prisma client initialization with logging and datasource configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["error", "warn"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Ensure this is set correctly
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
